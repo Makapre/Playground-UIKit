f736118cb837f1fe7c44af5dd1b61c771cb163ac
Marius Preikschat <m.preikschat@googlemail.com>
2023-09-07 10:14:12 +0200

    Initial Commit

---
+++ Playground UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Playground UIKit
+//
+//  Created by Marius Preikschat on 07.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
